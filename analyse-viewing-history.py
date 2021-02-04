9e7454b1e77a9c012d335bfde70f74336c0df41c
Andreas Fiehn <andreas@lyhnefiehn.dk>
2021-02-04 22:58:19 +0100

    Feat: Implemented NetflixMovie class and print result to csv file

--- analyse-viewing-history.py
+++ analyse-viewing-history.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import re
-import tmdb
+from tmdb import NetflixMovie
 from scrape_netflix import get_english_title
+import time
 
 # Get the api key from file
-API_KEY = tmdb.get_api_key_from_file('tmdb-api-key.txt')
+# API_KEY = tmdb.get_api_key_from_file('tmdb-api-key.txt')
 
 pd.set_option('display.max_rows', 1000)
 
@@ -34,81 +35,45 @@ def series_name_split(title):
 def set_runtime(row):
     # Check if it is a movie
     if row["type"] == "movie":
-        # Search TMDB for the title
-        result = tmdb.search_movie(row["Title"], API_KEY)
-
-        # If a result has been retrieved the runtime is set
-        if result:
-            runtime = result["runtime"]
-        else:
-            # # Netflix will be scraped for the English title
-            # english_title = get_english_title(row["Title"])
-            # # If an English title was found
-            # if english_title:    
-            #     # Search on TMDB again
-            #     result = tmdb.search_movie(english_title, API_KEY)
-            #     if result:
-            #         # If the result contains anything runtime will be set
-            #         runtime = result["runtime"]
-            #     else:
-            #         # If not runtime is undefined
-            #         runtime = None
-            # else: 
-            #     runtime = None
-            runtime = None
+        print(row["Title"])
+        movie = NetflixMovie(row["Title"],row["Date"])
+        movie.set_tmdb_api_key_from_file("tmdb-api-key.txt")
+        runtime = movie.get_movie_runtime(True)
     else:
-        title, episode, season = series_name_split(row["Title"])
+        pass
+        # title, episode, season = series_name_split(row["Title"])
         # imdb_id = tmdb.get_episode_imdb_id(API_KEY, title, episode, season)
 
         # if imdb_id:
         #     runtime = tmdb.get_imdb_runtime(imdb_id)
         # else: 
-        runtime = tmdb.get_series_runtime(API_KEY, title)
+        # runtime = tmdb.get_series_runtime(API_KEY, title)
         
     return runtime
+
+
+# track time spent
+time1 = time.time()
 
 file_path = "NetflixViewingHistory.csv"
 
 df = pd.read_csv(file_path)
 
-# ts = pd.Series(df['Title'].values)
-
 # Classify if it is a series or a movie based on the colons in the title
 df["type"] = ["series" if len(y.split(":")) >= 3 else "movie" for y in df["Title"]]
 
+# Only look at the movies
 df = df[df['type'] == "movie"]
 # df = df.iloc[:5,:]
 
-
+# Set the runtime column
 df["runtime"] = df.apply(set_runtime, axis=1)
-df = df.dropna()
+# df = df.dropna()
 
+# Print time it took
+print(time.time() - time1)
 
-print(sum(df["runtime"]))
+# Write results to a csv file
+df.to_csv("netflix_movies_runtime.csv")
 
-
-
-# for index, row in df.iterrows():
-#     # print(row["Title"])
-#     if row["type"] == "movie":
-#         result = tmdb.search_movie(row["Title"], tmdb.get_api_key_from_file('tmdb-api-key.txt'))
-#         if result:
-#             runtime = result["runtime"]
-#         else:
-#             runtime = None
-#     else:
-#         runtime = None
-    
-#     print(runtime)
-
-    # else:
-    #     print("Series")
-
-
-# df["series_name"] = ["S" if df["type"] == "series" else None for y in df["Title"]]
-
-
-
-title, episode, season = series_name_split("Spartacus: Blood and Sand: Vengeance: Wrath of the Gods")
-# print(title, episode, season)
-# print(df.head)
+print(df)
